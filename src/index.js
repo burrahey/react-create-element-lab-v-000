bcba04709a5c8a2af1affe2de9c348c1fa24e9d4
burrahey <burra.archana@gmail.com>
2018-01-06 14:35:21 -0500

    correct react code but npm having terrible issues

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+var meInReact = React.createElement('h1', {}, 'My First React Code');
+
+<div class="me">
+  <h1>An Awesome Person</h1>
+  <p>Who is learning React</p>
+
+  <ul class="my-interests">
+    <li>JavaScript</li>
+    <li>React</li>
+    <li>Movies</li>
+    <li>Ice cream</li>
+  </ul>
+</div>
+
+var liJ = React.createElement('li', {}, 'Javascript');
+var liR = React.createElement('li', {}, 'React');
+var liM = React.createElement('li', {}, 'Movies');
+var liI = React.createElement('li', {}, 'Ice cream');
+
+var h1 = React.createElement('h1', {}, "An Awesome Person");
+var p = React.createElement('p', {}, "Who is learning React");
+var ul = React.createElement('ul', { className = "my-interests" , key : '0'}, [ liJ,liR,liM,liI ] );
+
+var div = React.createElement('div', { className = "me" }, [h1, p, ul]);
+
+
 ReactDOM.render(
   meInReact,
   document.getElementById('global')
 );
 
 export default meInReact
-*/
