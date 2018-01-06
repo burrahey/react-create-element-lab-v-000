957d2c06b7b0f2754ed6f0c01ab04e13690021e1
burrahey <burra.archana@gmail.com>
2018-01-06 17:20:48 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var meInReact = React.createElement('h1', {}, 'My First React Code');
-
 <div class="me">
   <h1>An Awesome Person</h1>
   <p>Who is learning React</p>
@@ -15,16 +13,16 @@ var meInReact = React.createElement('h1', {}, 'My First React Code');
   </ul>
 </div>
 
-var liJ = React.createElement('li', {}, 'Javascript');
+var liJ = React.createElement('li', {}, 'JavaScript');
 var liR = React.createElement('li', {}, 'React');
 var liM = React.createElement('li', {}, 'Movies');
 var liI = React.createElement('li', {}, 'Ice cream');
 
 var h1 = React.createElement('h1', {}, "An Awesome Person");
 var p = React.createElement('p', {}, "Who is learning React");
-var ul = React.createElement('ul', { className = "my-interests" , key : '0'}, [ liJ,liR,liM,liI ] );
+var ul = React.createElement('ul', { className: "my-interests" , key : '0'}, [ liJ,liR,liM,liI ] );
 
-var div = React.createElement('div', { className = "me" }, [h1, p, ul]);
+var meInReact = React.createElement('div', { className: "me" }, [h1, p, ul]);
 
 
 ReactDOM.render(
